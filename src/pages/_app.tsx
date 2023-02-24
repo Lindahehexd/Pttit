4955539abfc4e4670a3adb7c138767aab4b7bdf2
Lindahehexd <tkd09152129@gmail.com>
2023-02-24 18:18:17 +0800

    set default theme to dark

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 RecoilEnv.RECOIL_DUPLICATE_ATOM_KEY_CHECKING_ENABLED = false;
 import type { AppProps } from "next/app";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, ColorModeScript  } from "@chakra-ui/react";
 import { theme } from "../chakara/theme";
 import { RecoilRoot } from "recoil";
 import { Router } from "next/router";
@@ -18,6 +18,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
       <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode="light" />
         <Layout isLoading={isLoading}>
           <Component {...pageProps} />
         </Layout>
